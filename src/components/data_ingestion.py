5e1bb2cdae34a4d749adb160f142dac6e2a84494
punnam swapna <punnamswapna13@gmail.com>
2024-10-19 00:14:02 +0530

    data ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,13 +4,11 @@ from src.pipeline.exception import CustomException
 from src.pipeline.logger import logging 
 import pandas as pd
 
+
+
 from sklearn.model_selection import train_test_split 
 from dataclasses import dataclass
 
-#from src.components.data_transformation import DataTransformation
-
-#from src.components.model_trainer import ModelTrainerConfig
-#from src.components.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
